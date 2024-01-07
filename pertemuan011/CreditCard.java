885a7c3b46f51224642407cc91fcad9f0e65c832
MuhZaidanRamdhan <muhammadzaidanramdhan509@gmail.com>
2024-01-07 18:27:30 +0700

    documentation

---
+++ pertemuan011/CreditCard.java
@@ -0,0 +1,7 @@
+package pertemuan011;
+
+public interface CreditCard {
+    public void giveBankDetails();
+
+    public String getCreditCard();
+}// End of the CreditCard interface.
---
+++ pertemuan012/proxy/RealInternet.java
@@ -0,0 +1,8 @@
+package pertemuan012.proxy;
+
+public class RealInternet implements Internet {
+    @Override
+    public void connectTo(String serverhost) {
+        System.out.println("Connecting to " + serverhost);
+    }
+}
